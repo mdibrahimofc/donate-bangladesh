2f3bcdddfdf2e509bba3df8fc1f3e6e161089e0c
mdibrahimofc <mdibrahim.ofc@gmail.com>
2024-09-24 09:56:21 +0600

    succesfully main part requiremnets finished

--- js/main.js
+++ js/main.js
@@ -69,7 +69,32 @@ document.getElementById('donate-submit-feni').addEventListener('click', function
     document.getElementById('main-balance').textContent = updateMainBalance;
     document.getElementById('feni-balance').textContent = updateNoakhaliBalance;
     document.getElementById('donate-amount-feni').value = '';
-    console.log({ mainBalance, noakhaliBalance, noakhaliDonate, updateMainBalance, updateNoakhaliBalance });
+    
+
+
+    // history data addes here 
+
+    const cardText = documentText('feni-text')
+    let div = createElement('div');
+    div.classList.add('p-8', 'rounded-md', 'shadow-md', 'space-y-4');
+
+    let historySection = id('history-section');
+    historySection.insertBefore(div, historySection.firstChild);
+
+    let h2 = document.createElement('h2');
+    h2.classList.add('font-bold', 'sm:text-3xl');
+    h2.innerHTML = `${noakhaliDonate} Taka is Donated for famine-2024 ${cardText}`
+    
+    div.appendChild(h2);
+
+    // show date functionality is here 
+    const date = new Date()
+    let p = document.createElement('p');
+    p.innerHTML = `Date: ${date}`
+    div.appendChild(p);
+
+
+
 })
 
     // quota movement card functionality 
@@ -94,7 +119,29 @@ document.getElementById('donate-submit-quota').addEventListener('click', functio
     document.getElementById('main-balance').textContent = updateMainBalance;
     document.getElementById('quota-balance').textContent = updateNoakhaliBalance;
     document.getElementById('donate-amount-quota').value = '';
-    console.log({ mainBalance, noakhaliBalance, noakhaliDonate, updateMainBalance, updateNoakhaliBalance });
+    
+    // history data addes here 
+
+    const cardText = documentText('quota-text')
+    let div = createElement('div');
+    div.classList.add('p-8', 'rounded-md', 'shadow-md', 'space-y-4');
+
+    let historySection = id('history-section');
+    historySection.insertBefore(div, historySection.firstChild);
+
+    let h2 = document.createElement('h2');
+    h2.classList.add('font-bold', 'sm:text-3xl');
+    h2.innerHTML = `${noakhaliDonate} Taka is Donated for famine-2024 ${cardText}`
+    
+    div.appendChild(h2);
+
+    // show date functionality is here 
+    const date = new Date()
+    let p = document.createElement('p');
+    p.innerHTML = `Date: ${date}`
+    div.appendChild(p);
+
+
 })
 
 
